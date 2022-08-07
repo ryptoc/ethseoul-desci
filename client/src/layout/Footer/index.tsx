53cc0562df5077e2323c15fcbcf8c24e97b93d7d
Sze Hao <ngszehao123@hotmail.com>
2022-08-07 19:05:03 +0800

    footer component

--- client/src/layout/Footer/index.tsx
+++ client/src/layout/Footer/index.tsx
@@ -1,7 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import Socials from '../Socials';
+
+const footerLinks = [
+    {
+        label: 'Contact',
+        to: '/contact',
+    },
+    {
+        label: 'FAQS',
+        to: '/faq',
+    },
+    {
+        label: 'Documentation',
+        to: '/documentation',
+    },
+];
+
 const Footer = () => {
     return (
         <footer>
-            <div className='container'></div>
+            <div className='container'>
+                <nav>
+                    {footerLinks.map(({ label, to }) => (
+                        <NavLink key={to} to={to}>
+                            {label}
+                        </NavLink>
+                    ))}
+                </nav>
+                <Socials />
+            </div>
         </footer>
     );
 };
